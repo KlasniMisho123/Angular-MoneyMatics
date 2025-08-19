f284568f2d0b313ce47c1529abedeb377968e542
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-08-19 19:36:30 +0400

    input component commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { InputComponent } from './input/input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, InputComponent],
 })
 export class AppComponent {}
