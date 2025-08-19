5827ccec7f09844b5a2430d670e8c2e0b7f22dac
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-08-19 20:11:41 +0400

    input component commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { InputComponent } from './input/input.component';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, InputComponent],
+  imports: [HeaderComponent, InputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {}
--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule  } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.css'
 })
 export class InputComponent {
 
+  initialInvestment = 0;
+
+  setData() {
+    console.log("initialInvestment: ", this.initialInvestment)
+  }
+
+  onSubmit() {
+    // console.log("submited")
+  }
 }
